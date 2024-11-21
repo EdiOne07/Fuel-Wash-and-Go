576608625c4819a36ebc32592fafc7c012e7db6d
MonkeyFonkey <andrevirlan@yahoo.com>
2024-11-21 19:09:41 +0200

    da

--- Server/src/controllers/userController.ts
+++ Server/src/controllers/userController.ts
@@ -98,7 +98,7 @@ export const loginUser = async (req: Request, res: Response): Promise<void> => {
         firebaseUid,
         email,
         username: userRecord.displayName || email,
-        location: '', // Set default or fetch from user input
+        location: '', 
       });
     }
 
