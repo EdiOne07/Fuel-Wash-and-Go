c64695db9245f63dc16cf38e63b0c06a74d265e7
MonkeyFonkey <andrevirlan@yahoo.com>
2024-12-01 17:37:03 +0200

    gata gagicu, merge backendu

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,20 +1,16 @@
 import express from 'express';
+import connectDB from './config/db';
 import userRoutes from './routes/userRoutes';
-import testRoute from './routes/testRoute'
-import * as dotenv from 'dotenv';
-dotenv.config({ path: '../.env' });
-
+import gasStationRoutes from './routes/gasStationRoutes'
 
 const app = express();
+connectDB();
 
 app.use(express.json());
 
 
 app.use('/api/users', userRoutes);
-app.use('/api', testRoute);
-
+app.use('/api/gas-stations',gasStationRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
